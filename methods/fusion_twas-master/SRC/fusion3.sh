4026f93a740c8904e494c367e5f4525b05e252fb
sgrzegorz <grzegorzsroka2@gmail.com>
2020-10-12 20:07:29 +0200

    postrocess fusion

---
+++ methods/fusion_twas-master/SRC/fusion3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ..
+
+input=./INPUT/input1
+output=./OUTPUT
+
+
+cat ${output}/PGC2.SCZ.22.dat | awk 'NR == 1 || $NF < 0.05/2058' > ${output}/PGC2.SCZ.22.top
+
+Rscript FUSION.post_process.R \
+--sumstats ${input}/PGC2.SCZ.sumstats \
+--input ${output}/PGC2.SCZ.22.top \
+--out ${output}/PGC2.SCZ.22.top.analysis \
+--ref_ld_chr ./LDREF/1000G.EUR. \
+--chr 22 \
+--plot --locus_win 100000

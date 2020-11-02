0d7300fc57dc4bef277c124524e27715c188a771
sgrzegorz <grzegorzsroka2@gmail.com>
2020-11-02 20:39:53 +0100

    goes further

--- methods/MetaXcan/SRC/harmonise.sh
+++ methods/MetaXcan/SRC/harmonise.sh
@@ -11,18 +11,25 @@ output=./OUTPUT
 # harmonize input gWAS to our reference
 echo harmonize input gWAS to our reference
 
+# output_column_map effect_allele effect_allele tutaj nie uzylem hm_effect_allele bo program sie gubil i widzial dwie kolumny effect_allele
 python $GWAS_TOOLS/gwas_parsing.py \
 -gwas_file /home/x/Downloads/GWAS2/27989323-GCST004430-EFO_0008173.h.tsv \
--output_column_map hm_variant_id variant_id \
 -output_column_map hm_other_allele non_effect_allele \
--output_column_map hm_effect_allele effect_allele \
+-output_column_map effect_allele effect_allele \
 -output_column_map hm_beta effect_size \
 -output_column_map p_value pvalue \
--output_column_map hm_chrom chromosome \
+-output_column_map chromosome chromosome \
+-output_column_map variant_id variant_id \
 -output_column_map standard_error standard_error \
---chromosome_format \
 -output_column_map hm_pos position \
 -output_column_map hm_effect_allele_frequency frequency \
--output_order variant_id chromosome position effect_allele non_effect_allele frequency pvalue effect_size standard_error \
+--insert_value sample_size 9605470 --insert_value n_cases 9605470 \
+--chromosome_format \
+-verbosity 1 \
+-output_order variant_id panel_variant_id chromosome position effect_allele non_effect_allele frequency pvalue zscore effect_size standard_error sample_size n_cases \
 -output ${output}/harmonized_gwas/CARDIoGRAM_C4D_CAD_ADDITIVE.txt.gz
 
+#-output_order variant_id chromosome position effect_allele non_effect_allele frequency pvalue effect_size standard_error \
+#--insert_value sample_size 184305 --insert_value n_cases 60801 \
+#-output_column_map hm_effect_allele effect_allele \
+#-output_order variant_id panel_variant_id chromosome position effect_allele non_effect_allele frequency pvalue zscore effect_size standard_error sample_size n_cases \

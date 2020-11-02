e206e7b9a4bddd18e5983930c6f61b6ded1e3eb5
sgrzegorz <grzegorzsroka2@gmail.com>
2020-11-02 23:01:30 +0100

    wykresy fusion metaxcan pvalue

--- backend/metaxcan_print_chart.py
+++ backend/metaxcan_print_chart.py
@@ -8,7 +8,8 @@ import pandas as pd
 
 columns = ["gene","before","after"]
 dtypes ={"gene": str,"before":float,"after":float}
-data = pd.read_csv('pictures/fusion_before_after.csv', usecols=columns,dtype=dtypes,sep='\t',header=(0))
+data = pd.read_csv('pictures/metaxcan_before_after.csv', usecols=columns,dtype=dtypes,sep='\t',header=(0))
+
 data =data.sort_values(by=['gene'])
 
 y = []
@@ -17,8 +18,7 @@ for index, row in data.iterrows():
     y.append(log)
 
 
-
-title = 'Fusion pvalue comparison'
+title = 'Metaxcan pvalue comparison'
 labels =dict(x="gene", y="| log| initialPvalue - finalPvalue ||")
 fig = px.bar(x=list(data.gene), y =y, labels=labels,title=title)
 fig.update_xaxes(type='category')

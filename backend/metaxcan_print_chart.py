3dab9de0b2323ffdbd1f2b3cbb6007dc82107d13
domguz <36860212+domguz@users.noreply.github.com>
2020-11-28 23:43:11 +0100

    Add files via upload

--- backend/metaxcan_print_chart.py
+++ backend/metaxcan_print_chart.py
@@ -1,32 +1,45 @@
 import math
-
+from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
 import plotly.express as px
 import pandas as pd
 # df = px.data.tips()
 # fig = px.histogram(df, x="total_bill")
-from definitions import ROOT_DIR
+#from definitions import ROOT_DIR
 import sys
 
-# chart_file=f'{ROOT_DIR}/output/metaxcan_before_after.csv'
-chart_file=sys.argv[1]
 
 
-columns = ["gene","before","after"]
-dtypes ={"gene": str,"before":float,"after":float}
-data = pd.read_csv(chart_file, usecols=columns,dtype=dtypes,sep='\t',header=(0))
 
-data =data.sort_values(by=['gene'])
+class Widget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.browser = QtWebEngineWidgets.QWebEngineView(self)
 
-y = []
-for index, row in data.iterrows():
-    log = abs(math.log(abs(row['before']-row['after'])))
-    y.append(log)
+        vlayout = QtWidgets.QVBoxLayout(self)
+        vlayout.addWidget(self.browser)
 
+        self.resize(1000,800)
+        self.show_graph()
 
-title = 'Metaxcan pvalue comparison'
-labels =dict(x="gene", y="| log| initialPvalue - finalPvalue ||")
-fig = px.bar(x=list(data.gene), y =y, labels=labels,title=title)
-fig.update_xaxes(type='category')
-fig.show()
+    def show_graph(self):
+        # chart_file=f'{ROOT_DIR}/output/metaxcan_before_after.csv'
+        chart_file=sys.argv[1]
+        columns = ["gene","before","after"]
+        dtypes ={"gene": str,"before":float,"after":float}
+        data = pd.read_csv(chart_file, usecols=columns,dtype=dtypes,sep='\t',header=(0))
+        data =data.sort_values(by=['gene'])
+        y = []
+        for index, row in data.iterrows():
+            log = abs(math.log(abs(row['before']-row['after'])))
+            y.append(log)
+        title = 'Metaxcan pvalue comparison'
+        labels =dict(x="gene", y="| log| initialPvalue - finalPvalue ||")
+        fig = px.bar(x=list(data.gene), y =y, labels=labels,title=title)
+        fig.update_xaxes(type='category')
+        self.browser.setHtml(fig.to_html(include_plotlyjs='cdn'))
 
-
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    widget = Widget()
+    widget.show()
+    app.exec()

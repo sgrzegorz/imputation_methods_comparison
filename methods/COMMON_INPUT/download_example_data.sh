bf0b043e95fafa32256513dca4250da7326c126b
sgrzegorz <grzegorzsroka2@gmail.com>
2020-12-22 10:07:44 +0100

    download

--- methods/COMMON_INPUT/download_example_data.sh
+++ methods/COMMON_INPUT/download_example_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-fileid="1qTU90yhlsVphiWK2u3ey6KXHIio1kR5J"
-filename="helpme.tar.gz"
+fileid="15S2vuhv55tUyLFxb4fGtwDP88ry9-FpS"
+filename="common_input.tar.gz"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-tar -xvzf helpme.tar.gz
-rm -r helpme.tar.gz
+tar -xvzf common_input.tar.gz
+rm -r common_input.tar.gz
--- methods/METAXCAN/INPUT/download_metaxcan_example_data.sh
+++ methods/METAXCAN/INPUT/download_metaxcan_example_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
-URL=
-
-wget
+fileid="1akRXhGhwc97FgMolN1wUMiivwTiJxiH5"
+filename="metaxcan_example_data.tar.gz"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+tar -xvzf metaxcan_example_data.tar.gz
+rm -r metaxcan_example_data.tar.gz
